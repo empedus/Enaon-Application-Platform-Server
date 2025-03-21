0e7ddad167a4418b86aa8266e18f00046c72e58b
giann <giannakiss2003@gmail.com>
2025-03-21 16:23:06 +0200

    upload attachment fixes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const authController = require("../controllers/authController")
 
 // 1. Authenticate user and generate JWT token
-router.get("/user_auth", authController.authenticateUser)
+//router.get("/user_auth", authController.authenticateUser)
 
 // Microsoft SSO authentication route
 router.post("/auth/microsoft", authController.authenticateUser);

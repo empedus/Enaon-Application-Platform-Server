becb11d820f8aa8c79c7aa7a1e4975ea477b12bd
giann <giannakiss2003@gmail.com>
2025-04-02 16:11:41 +0300

    middleware aadded

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express")
 const router = express.Router()
 const authController = require("../controllers/authController")
-
+const { authorizeMeterApp } = require("../middleware/auth")
 // 1. Authenticate user and generate JWT token
 //router.get("/user_auth", authController.authenticateUser)
 
 // Microsoft SSO authentication route
 router.post("/auth/microsoft", authController.authenticateUser);
-router.post("/auth/update_username_pass", authController.updateUsernamePass)
+router.post("/auth/update_username_pass",authorizeMeterApp, authController.updateUsernamePass)
 module.exports = router
 
